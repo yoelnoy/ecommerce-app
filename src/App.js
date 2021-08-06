a8eb6e4453dcd5f7c22470cd083d202d5d719ada
Yoel Noy <53430988+yoelnoy@users.noreply.github.com>
2021-08-06 09:37:14 +0200

    adding loader spinner

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import { commerce } from './lib/Commerce';
 import { Products, Navbar, Cart } from './components'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Loader from 'react-spinners/ClipLoader';
 // import { log } from 'async';
 
 function App() {
 
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({})
+  const [loader, setLoader] = useState(true)
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
     setProducts(data);
+    setLoader(false);
   }
 
   const fetchCart = async () => {
@@ -48,15 +51,23 @@ function App() {
   return (
     <Router>
       <div className="App">
+
         <Navbar totalItems={cart.total_items} />
-        <Switch>
-          <Route exact path="/">
-            <Products products={products} onAddToCart={handleAddToCart} />
-          </Route>
-          <Route exact path="/cart">
-            <Cart cart={cart} handleUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart} handleEmptyCart={handleEmptyCart} />
-          </Route>
-        </Switch>
+        {loader ? (
+          <div className="app__loader">
+            <Loader color="rgb(85, 236, 192)" loading={loader} size={350} />
+          </div>
+        ) : (
+          <Switch>
+            <Route exact path="/">
+              <Products products={products} onAddToCart={handleAddToCart} />
+            </Route>
+            <Route exact path="/cart">
+              <Cart cart={cart} handleUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart} handleEmptyCart={handleEmptyCart} />
+            </Route>
+          </Switch>
+        )}
+
       </div>
     </Router>
   );
